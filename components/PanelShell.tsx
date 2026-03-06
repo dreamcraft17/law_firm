f283003ce4033f5af2073a9663b428ed6309841e
dreamcraft17 <85624016+dreamcraft17@users.noreply.github.com>
2026-03-06 13:31:12 +0700

    perbaikan bg2

--- components/PanelShell.tsx
+++ components/PanelShell.tsx
@@ -32,7 +32,7 @@ export default function PanelShell(props: { children: React.ReactNode }) {
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0">
         <Header title={title} />
-        <main className="flex-1 p-6 overflow-auto bg-white">
+        <main className="flex-1 p-6 overflow-auto bg-white text-slate-900 panel-content-light">
           <div className="max-w-7xl mx-auto">
             {props.children}
           </div>
